c9bc968509735c2e9241fd24782d8a764454e424
Alex Alemzadeh <alexalemzadeh@Alexs-MacBook-Pro.local>
2021-04-23 17:27:56 -0700

    Update Users page to indicate if there are no users registered on the site

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -5,28 +5,31 @@ import API from '../utils/API';
 
 export default class Users extends Component {
   state = {
-    users: []
+    users: [],
+    loaded: 'false',
   }
 
   componentDidMount() {
     console.log("This.state.users", this.state.users)
     API.getUsers().then(res => {
       this.setState({
-        users: res.data
+        users: res.data,
+        loaded: 'true'
       })
     })
   }
 
   render() {
-    console.log("This.state.users", this.state.users)
+    const { users, loaded } = this.state;
+    console.log("This.state.users", users)
     return (
       <div className="container">
         <h2 style={{fontSize:"20px", margin:"10px 0"}}>Users</h2>
-        {this.state.users.length && 
-          this.state.users.map(user => (
+        {loaded ? (users.length && 
+          users.map(user => (
             <UserCard key={`user${user.id}`} user={user} />
             // <p><Link to={`/users/${user.username}`}>@{user.username}</Link></p>
-        )) || <p>Fetching users...</p>}
+        )) || <p>There are currently no users</p>) : <p>Fetching users...</p>}
       </div>
     )
   }
